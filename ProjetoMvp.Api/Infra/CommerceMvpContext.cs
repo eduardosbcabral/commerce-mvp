40d3267e9368ef4013d58a3f37873463bd552d6c
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-10-04 23:41:20 -0300

    Configure database and entity mapping

---
+++ ProjetoMvp.Api/Infra/CommerceMvpContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMvp.Api.Models;
+
+namespace ProjetoMvp.Api.Infra
+{
+    public class CommerceMvpContext : DbContext
+    {
+        public DbSet<Commerce> Commerces { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Site> Sites { get; set; }
+
+        public CommerceMvpContext(DbContextOptions<CommerceMvpContext> options) :
+            base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Commerce>()
+                .ToTable("TB_COMMERCE")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Commerce>()
+                .HasOne(x => x.Site)
+                .WithOne(x => x.Commerce)
+                .HasForeignKey<Site>(b => b.Id);
+
+            modelBuilder.Entity<Commerce>()
+                .HasOne(x => x.Address);
+
+            modelBuilder.Entity<Product>()
+                .ToTable("TB_PRODUCT")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(x => x.Images);
+
+            modelBuilder.Entity<ProductImage>()
+                .ToTable("TB_PRODUCT_IMAGE")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Site>()
+                .ToTable("TB_SITE")
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Address>()
+                .ToTable("TB_ADDRESS")
+                .HasKey(x => x.Id);
+        }
+    }
+}
--- ProjetoMvp.Api/Startup.cs
+++ ProjetoMvp.Api/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProjetoMvp.Api.Infra;
 
 namespace ProjetoMvp.Api
 {
@@ -26,6 +28,8 @@ namespace ProjetoMvp.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<CommerceMvpContext>(options => options.UseNpgsql(Configuration.GetConnectionString("Database")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
