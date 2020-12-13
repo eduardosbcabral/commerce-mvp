11cdb6e97b55e09706d72e977460be73b0089895
Eduardo Cabral <eduardosbcabral@gmail.com>
2020-12-13 03:01:36 -0300

    Add contract validation in Address model

--- ProjetoMvp.Api/Models/Address.cs
+++ ProjetoMvp.Api/Models/Address.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
 
 namespace ProjetoMvp.Api.Models
 {
-    public class Address
+    public class Address : Notifiable
     {
         public Guid Id { get; private set; }
         public string Street { get; private set; }
@@ -10,5 +12,30 @@ namespace ProjetoMvp.Api.Models
         public string State { get; private set; }
         public string ZipCode { get; private set; }
         public string Country { get; private set; }
+
+        public Address(string country, string state, string city)
+        {
+            AddNotifications(new Contract()
+                .IsNotNull(country, nameof(Country), "País é obrigatório.")
+                .HasMinLen(country, 3, nameof(Country), "País deve ter no mínimo 3 caracteres.")
+                .HasMaxLen(country, 50, nameof(Country), "País deve ter no máximo 50 caracteres.")
+
+                .IsNotNull(state, nameof(State), "Estado é obrigatório.")
+                .HasMinLen(state, 3, nameof(State), "Estado deve ter no mínimo 3 caracteres.")
+                .HasMaxLen(state, 50, nameof(State), "Estado deve ter no máximo 50 caracteres.")
+
+                .IsNotNull(city, nameof(City), "Cidade é obrigatória.")
+                .HasMinLen(city, 3, nameof(City), "Cidade deve ter no mínimo 3 caracteres.")
+                .HasMaxLen(city, 50, nameof(City), "Cidade deve ter no máximo 50 caracteres.")
+            );
+
+            if(Valid)
+            {
+                Id = Guid.NewGuid();
+                Country = country;
+                State = state;
+                City = city;
+            }
+        }
     }
 }
